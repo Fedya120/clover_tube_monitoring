3a0d8364a655cdf3dcdf2885d95b07ab81eef90c
Fedya120 <fdrobysevskij@gmail.com>
2025-12-07 18:41:04 +0300

    2.0 version of WorldChanger

--- WorldChanger.py
+++ WorldChanger.py
@@ -4,6 +4,8 @@ from string import Template
 
 from random import uniform
 
+import math
+
 WORLD_INCLUDE = Template('''
     <include>
       <uri>model://${model_name}</uri>
@@ -50,9 +52,28 @@ x_zero = uniform(1.2, 3.0)
 period = 0
 world = load_world(r'/home/clover/catkin_ws/src/clover/clover_simulation/resources/worlds/clover_aruco1.world')
 world = add_model(world, 'big_tube_with_angle', x = 4.0, y = 1.0, z = 0.1, roll = 0, pitch = 0, yaw = 0)
-for num in range(1, 6):
-    world = add_model(world, 'mini_pipe' + str(num), x = x_zero + period, y = 1.0, z = 0.3, roll = 0, pitch = 0, yaw = 0)
-    period += uniform(0.75, 0.9)
+
+random_floats = []
+current_value = 1
+next_value = 0
+cnt = 0
+while len(random_floats) < 5:
+    next_value = current_value + uniform(0.75, (8.5 - current_value))
+    cnt += 1
+    if next_value <= 9 - (0.95 * (5 - cnt)):
+        random_floats.append(next_value)
+        current_value = next_value
+    else:
+        cnt -= 1
+
+cnt = 0
+for float in random_floats:
+    cnt += 1
+    if float <= 7:
+        world = add_model(world, 'mini_pipe' + str(cnt), x = float, y = 1.0, z = 0.3, roll = 0, pitch = 0, yaw = 0)
+    else:
+        world = add_model(world, 'mini_pipe' + str(cnt), x = float, y = 1.0 + math.tan(0.2) * (float - 7), z = 0.3, roll = 0, pitch = 0, yaw = 0)
 with open(r'/home/clover/catkin_ws/src/clover/clover_simulation/resources/worlds/clover_aruco.world', 'w') as f:
     save_world(world, f)
 print('gotovo')
+'''+ math.tan(0.2) * float'''
