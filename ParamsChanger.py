287387f43e9ccf27d7b010eb298a28d18808a217
Fedya120 <fdrobysevskij@gmail.com>
2025-12-07 19:01:14 +0300

    2.0 version of ParamsChanger

--- ParamsChanger.py
+++ ParamsChanger.py
@@ -17,4 +17,24 @@ modify_arg_value(file_path, 'rangefinder_vl53l1x', 'true')
 file_path = '/home/clover/catkin_ws/src/clover/clover/launch/aruco.launch'
 modify_arg_value(file_path, 'aruco_map', 'true')
 modify_arg_value(file_path, 'aruco_vpe', 'true')
+
+import subprocess
+import os
+
+package_name = 'aruco_pose'
+node_name = 'genmap.py'
+args = ['0.335', '10', '10', '1', '1', '0']
+output_file = os.path.expanduser('~/catkin_ws/src/clover/aruco_pose/map/map.txt')
+additional_args = ['--top-left']
+
+command = ['rosrun', package_name, node_name] + args + additional_args
+
+with open(output_file, 'w') as f:
+    try:
+        subprocess.run(command, stdout=f, stderr=subprocess.PIPE, check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+        print(e.stderr.decode())
+
+
 print("Done")
